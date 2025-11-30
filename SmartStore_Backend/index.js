0a21d43637c53763c336fbc3473bd78449e16e40
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2025-11-30 23:52:00 +0530

    new

--- SmartStore_Backend/index.js
+++ SmartStore_Backend/index.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// --- Database Connection ---
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log(' MongoDB Connected Successfully'))
-    .catch(err => console.error(' MongoDB Connection Error:', err));
+// Connect MongoDB
+connectDB();
 
-// --- Routes ---
+// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/stores', require('./routes/storeRoutes')); // Admin Only
 app.use('/api/products', require('./routes/productRoutes'));
@@ -23,9 +22,9 @@ app.use('/api/sales', require('./routes/saleRoutes'));
 app.use('/api/purchases', require('./routes/purchaseRoutes'));
 app.use('/api/finance', require('./routes/transactionRoutes')); // Receipts & Payments
 
-// --- Health Check ---
+// Health Check
 app.get('/', (req, res) => res.send('SmartStore API is Running...'));
 
 app.listen(PORT, () => {
-    console.log(` Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
