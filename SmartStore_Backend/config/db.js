44d0d20a22ac440b097c63c13e3be781b144b9b8
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2025-11-30 23:56:58 +0530

    Update db.js

--- SmartStore_Backend/config/db.js
+++ SmartStore_Backend/config/db.js
@@ -1,16 +1,11 @@
-// config/db.js - MongoDB connection using mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.error('MongoDB Connection Error:', err.message);
+    console.error("MongoDB Connection Error:", err.message);
     process.exit(1);
   }
 };
