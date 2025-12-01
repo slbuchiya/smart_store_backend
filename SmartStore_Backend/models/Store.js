ef758c744c428506dcf05d5531274ab409e40fe1
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2025-12-01 09:11:06 +0530

    Format Store.js for consistency and readability

--- SmartStore_Backend/models/Store.js
+++ SmartStore_Backend/models/Store.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const storeSchema = new mongoose.Schema({
-    storeId: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    storeName: { type: String, required: true },
-    ownerName: { type: String, required: true },
-    mobile: { type: String, required: true },
-    email: String,
-    address: String,
-    gst: String,
-    planType: { type: String, default: 'Yearly' },
-    expiryDate: { type: Date, required: true },
-    status: { type: String, default: 'active', enum: ['active', 'suspended'] }
+  storeId: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  storeName: { type: String, required: true },
+  ownerName: { type: String, required: true },
+  mobile: { type: String, required: true },
+  email: String,
+  address: String,
+  gst: String,
+  planType: { type: String, default: 'Yearly' },
+  expiryDate: { type: Date, required: true },
+  status: { type: String, default: 'active', enum: ['active', 'suspended'] }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Store', storeSchema);
