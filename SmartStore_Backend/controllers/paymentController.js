223271099bbaf6a9a1d15fbbd5eab870450c1e80
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2025-12-22 01:35:31 +0530

    22/12/25 1:35

---
+++ SmartStore_Backend/controllers/paymentController.js
@@ -0,0 +1,33 @@
+const Payment = require('../models/Payment');
+
+// List
+exports.list = async (req, res) => {
+    try {
+        const list = await Payment.find({ storeId: req.storeId }).sort({ date: -1 });
+        res.json(list);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// Create
+exports.create = async (req, res) => {
+    try {
+        const partyName = req.body.partyName || req.body.name || req.body.supplierName || 'Unknown';
+        const payment = new Payment({ ...req.body, partyName, storeId: req.storeId });
+        await payment.save();
+        res.status(201).json(payment);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Remove
+exports.remove = async (req, res) => {
+    try {
+        await Payment.findOneAndDelete({ _id: req.params.id, storeId: req.storeId });
+        res.json({ message: 'Deleted' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
