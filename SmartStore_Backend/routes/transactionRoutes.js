32fa560906c117a2c1b00ae31a96c9bf2e16a3d2
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2025-12-22 01:13:46 +0530

    22/12/25 1:13am

--- SmartStore_Backend/routes/transactionRoutes.js
+++
@@ -1,14 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/transactionController');
-const auth = require('../middleware/auth');
-
-// Protect all routes
-router.use(auth);
-
-// Routes
-router.get('/', controller.list);
-router.post('/', controller.create);
-
-// 👇 આ લાઈન ખાસ હોવી જોઈએ! (જો અહીં exports.list લખ્યું હોય તો ભૂલ છે)
-module.exports = router;
