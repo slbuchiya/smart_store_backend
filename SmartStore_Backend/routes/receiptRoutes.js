2607ac643a401829e6a4d7f70d5a0d41a0a0799c
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2025-12-22 01:31:10 +0530

    22/12/25 1:31

--- SmartStore_Backend/routes/receiptRoutes.js
+++ SmartStore_Backend/routes/receiptRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+// 👇 આ પાથ સાચો હોવો જોઈએ અને સ્પેલિંગ મિસ્ટેક ન હોવી જોઈએ
 const controller = require('../controllers/receiptController');
 const auth = require('../middleware/auth');
 
 router.use(auth);
 
+// 👇 જો controller.list undefined હોય તો જ એરર આવે
 router.get('/', controller.list);
 router.post('/', controller.create);
 router.delete('/:id', controller.remove);
 
-// 👇 આ લાઈન ખાસ હોવી જોઈએ!
 module.exports = router;
