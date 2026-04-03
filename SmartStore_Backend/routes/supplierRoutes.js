d14f0ddb3813355a96f24f045abbc280c50b5689
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2026-04-03 12:37:08 +0530

    purchase update

--- SmartStore_Backend/routes/supplierRoutes.js
+++ SmartStore_Backend/routes/supplierRoutes.js
@@ -4,8 +4,12 @@ const controller = require('../controllers/supplierController');
 const auth = require('../middleware/auth');
 
 router.use(auth);
+
 router.get('/', controller.list);
 router.post('/', controller.create);
+
+router.put('/:id', controller.update);
+
 router.delete('/:id', controller.remove);
 
 module.exports = router;
